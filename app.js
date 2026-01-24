c451b8633732bdcdbb5eb729b43c4613bd70c9e2
chie0349ja <momochie0712@gmail.com>
2026-01-24 10:53:37 +0900

    expressディレクトリの更新

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/Hello');
+var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var nytRouter = require('./routes/nyt');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 
 var app = express();
@@ -20,6 +21,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
+app.use('/nyt', nytRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,10 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
-app.use('/cat', catRouter);
-app.use('/nyt', nytRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,5 +51,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
